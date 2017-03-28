e135439d4ce2ed32353c0c8806dff702cb5d37e7
andrii_vlasiuk <vlasiuk.andrii93@gmail.com>
2017-03-28 23:50:14 +0300

    First test works

--- src/main/java/LogInPage.java
+++ src/main/java/LogInPage.java
@@ -1,22 +1,26 @@
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class LogInPage {
-    private WebDriver ldriver;
+import static java.lang.Thread.sleep;
+
+public class LogInPage extends FunctionExtension{
+    private WebDriver driver;
     public LogInPage(WebDriver driver) {
-        this.ldriver = driver;
+        this.driver = driver;
+        System.out.println("driver in constructor=" + driver);
+
     }
 
     @FindBy(css = "input#Email")
     private WebElement emailField;
 
-    @FindBy(id = "Next")
+    @FindBy(id = "next")
     private WebElement nextButton;
 
     @FindBy(id = "Passwd")
@@ -25,22 +29,32 @@ public class LogInPage {
     @FindBy(id = "signIn")
     private WebElement signInButton;
 
-    @FindBy(css = "a[title*=vlasiukandrii39@gmail.com]")
+    @FindBy(css = "a.gb_b.gb_eb.gb_R")
     private WebElement accountLink;
 
-
-
-
-    public void logIn(){
-        System.out.println("driver on page=" + ldriver);
+    public void logIn() throws InterruptedException {
+        System.out.println("driver on page=" + driver);
         emailField.sendKeys("vlasiuk.andrii39@gmail.com");
         nextButton.click();
+        //waitForJSinactivity(driver);
+        sleep(3000);
         passwordField.sendKeys("3141592653531415926535");
         signInButton.click();
-        ldriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        sleep(5000);
     }
+    /* ну вобщем веб элементы я проинициализировал,
+    дальше он не находит элемент пассворд,
+   как я понял потому что после клацанья на кнопку далее кнопка пассворд появляется не моментально,
+   а клацанье на кнопку логин происходит сразу же после нажатия на кнопку далее
+    * завтра звони
+    * спок
+    */
 
     public void assertIsAuthorised(){
         accountLink.isDisplayed();
+    }
+
+    public void init(WebDriver driver){
+        PageFactory.initElements(driver, this);
     }
 }
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
 
 import static java.lang.Thread.sleep;
 
@@ -8,8 +10,10 @@ public class FirstTest extends BaseSpec {
     LogInPage logInPage = new LogInPage(driver);
 
     @Test
-    public void logInTest(){
+    public void logInTest() throws InterruptedException {
         System.out.println("driver in test=" + driver);
+        //driver.findElement(By.cssSelector("input#Email")).sendKeys("ljnsofvnorvn");
+        logInPage.init(driver);
         logInPage.logIn();
         logInPage.assertIsAuthorised();
     }
